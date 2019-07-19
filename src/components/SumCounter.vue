be159d450f2ef82a4891c165eed0227d1ef113be
bbillymai <1363543881@qq.com>
2019-07-19 17:38:33 +0800

    create sumCounter

---
+++ src/components/SumCounter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <span></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SumCounter"
+    }
+
+</script>
+
+<style scoped>
+
+</style>
