0a574c3693a4e0b8acc4100e1e572a93b6ecde42
bbillymai <1363543881@qq.com>
2019-07-19 17:38:02 +0800

    finished counter Group

---
+++ src/components/CountGroup.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <div v-for="index in num" :key="index">
+        <Counter></Counter>
+    </div>
+    </div>
+</template>
+
+import Counter from "./components/Counter"
+
+<script>
+    import Counter from "./Counter";
+    export default {
+        name: "CountGroup",
+        components: {Counter},
+        props:{
+            num:Number
+        },
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
